d1ec43a65e357f82b80a1f2ecbb5d0374da87a68
iqhater <iqhater@yandex.ru>
2022-02-04 18:56:29 +0300

    add search tests.

--- api/search_test.go
+++ api/search_test.go
@@ -1,8 +1,10 @@
 package api
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-// TODO:
 func TestValidBuildSuffixTree(t *testing.T) {
 	l := Locations{}
 
@@ -10,5 +12,91 @@ func TestValidBuildSuffixTree(t *testing.T) {
 
 	if l.Tree == nil {
 		t.Errorf("BuildSuffixTree must be valid tree! got: %v", l.Tree)
+	}
+}
+
+func TestValidWithDataBuildSuffixTree(t *testing.T) {
+
+	l := Locations{
+		Locations: []Location{
+			{
+				Region: "Punta Cana",
+				Hotels: []string{"Riu Bavaro", "Hotel Bavaro", "Resort Beach 5"},
+			},
+			{
+				Region: "Macao",
+				Hotels: []string{"Beach Summer 4", "Macao Beach Resort", "Star Hotel Resort"},
+			},
+		},
+		// Tree:       nil,
+		// HotelsList: nil,
+		// Result:     nil,
+	}
+
+	l.BuildSuffixTree()
+
+	result := map[string][]string{
+		"Punta Cana": {"Riu Bavaro", "Hotel Bavaro", "Resort Beach 5"},
+		"Macao":      {"Beach Summer 4", "Macao Beach Resort", "Star Hotel Resort"},
+	}
+
+	// check valid regions in tree
+	for _, location := range l.Locations {
+		if _, ok := result[location.Region]; !ok {
+			t.Errorf("BuildSuffixTree must have a valid regions! got: %v", location)
+		}
+	}
+
+	// check valid hotels in tree
+	for _, location := range l.Locations {
+
+		if _, ok := result[location.Region]; ok {
+			for index, h := range result[location.Region] {
+				if h != location.Hotels[index] {
+					t.Errorf("BuildSuffixTree must have a valid hotels! got: %v", h)
+				}
+			}
+		}
+	}
+
+	if l.Tree == nil {
+		t.Errorf("BuildSuffixTree must be valid tree! got: %v", l.Tree)
+	}
+}
+
+func TestSuccessSearchInSuffixTree(t *testing.T) {
+
+	l := Locations{
+		Locations: []Location{
+			{
+				Region: "Punta Cana",
+				Hotels: []string{"Riu Bavaro", "Hotel Bavaro", "Resort Beach 5"},
+			},
+			{
+				Region: "Macao",
+				Hotels: []string{"Beach Summer 4", "Macao Beach Resort", "Star Hotel Resort"},
+			},
+		},
+	}
+
+	text := "ea"
+	limit := 10
+
+	l.BuildSuffixTree()
+	result := l.SearchInSuffixTree(text, limit)
+
+	type ResultHotels struct {
+		Hotels map[string][]string
+	}
+
+	validResult := ResultHotels{
+		Hotels: map[string][]string{
+			"Punta Cana": {"Resort Beach 5"},
+			"Macao":      {"Macao Beach Resort", "Beach Summer 4"},
+		},
+	}
+
+	if !reflect.DeepEqual(result.Hotels, validResult.Hotels) {
+		t.Errorf("Search results must have a valid regions and hotels! got: %v", result)
 	}
 }
