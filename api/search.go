78f1107572b3db5a8da269d5df882ba0bf097e71
iqhater <iqhater@yandex.ru>
2022-05-24 17:07:13 +0300

    refactor locations structures into search.go file.

--- api/search.go
+++ api/search.go
@@ -6,6 +6,25 @@ import (
 	"hotels_api/suffixtree"
 )
 
+// Location struct store Region and slice of hotels
+type Location struct {
+	Region string   `json:"region"`
+	Hotels []string `json:"hotels"`
+}
+
+// Locations struct store slice of Locations and Counter struct
+type Locations struct {
+	Locations  []Location `json:"locations"`
+	Tree       *suffixtree.GeneralizedSuffixTree
+	HotelsList []string
+	Result     []string
+}
+
+// FilteredHotels struct store output response hotels data
+type FilteredHotels struct {
+	Hotels map[string][]string `json:"hotels"`
+}
+
 // BuildSuffixTree method create a new search tree with regions and hotels
 func (l *Locations) BuildSuffixTree() {
 
